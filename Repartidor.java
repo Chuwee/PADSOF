d8ea31975f5c467cb09f31a0d85f95d8eedbe7a3
pinar02 <91214411+pinar02@users.noreply.github.com>
2022-03-22 19:48:13 +0100

    Add files via upload

---
+++ Repartidor.java
@@ -0,0 +1,32 @@
+/**
+ * @author Paloma Ballester Asesio, Ignacio Ildefonso del Miguel Ruano y María del Pinar Sacristán Matesanz
+ * 
+ */
+
+public class Repartidor extends UsuarioIdentificado{
+    private String telefono;
+
+    public Repartidor(String telefono, String NIF, String Nombre, String Usuario, String Contrasena, String email){
+        super(NIF, Nombre, Usuario, Contrasena, email);
+        this.telefono = telefono;
+
+    }
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    public void setTelefono(String tel) {
+        this.telefono = tel;
+    }
+
+    public void imprimir_informe(){
+
+    }
+    public void marcar_Paquetes_entregados(){
+
+    }
+    public Camion ver_camion_asignado(){
+        
+    }
+    
+}
