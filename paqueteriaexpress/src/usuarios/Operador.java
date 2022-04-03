afc207ea22f5b8852aaa0f1fd90a31502a1d1b3f
paloba22 <pbasesio@gmail.com>
2022-04-03 13:22:32 +0200

    empaquetar

--- paqueteriaexpress/src/usuarios/Operador.java
+++ paqueteriaexpress/src/usuarios/Operador.java
@@ -151,7 +151,7 @@ public class Operador extends UsuarioIdentificado{
     		if(u.isFragil()) {
     			Producto prod=(Producto)u;
     			for(int i=0;i<prod.getUnidades();i++) {
-    				Paquete p=new Paquete(id, u.getDireccion());
+    				Paquete p=new Paquete(id, u.getDireccion(), TipoPaquete.FRAGIL);
         			this.empaquetar(u, p);
         			sist.getPaquetes().add(p);
         			id++;
@@ -160,7 +160,7 @@ public class Operador extends UsuarioIdentificado{
     		}else if(u.isLote()) {
     			Lote lote=(Lote)u;
     				if(lote.getTipopaquete().equals(TipoPaquete.FRAGIL)) {
-    					Paquete p=new Paquete(id, u.getDireccion());
+    					Paquete p=new Paquete(id, u.getDireccion(), TipoPaquete.FRAGIL);
             			this.empaquetar(u, p);
             			sist.getPaquetes().add(p);
             			id++;
@@ -170,15 +170,15 @@ public class Operador extends UsuarioIdentificado{
     		}
     	}
     	while(num_empaquetado!=pedido.getUnidades().size()) {
-    		Paquete p_estandar = new Paquete(id, pedido.getDireccion());
+    		Paquete p_estandar = new Paquete(id, pedido.getDireccion(), TipoPaquete.ESTANDAR);
     		id++;
-    		Paquete p_congelado = new Paquete(id, pedido.getDireccion());
+    		Paquete p_congelado = new Paquete(id, pedido.getDireccion(), TipoPaquete.CONGELADO);
     		id++;
-    		Paquete p_refrigerado = new Paquete(id, pedido.getDireccion());
+    		Paquete p_refrigerado = new Paquete(id, pedido.getDireccion(), TipoPaquete.REFRIGERADO);
     		id++;
-    		Paquete p_dim_esp = new Paquete(id, pedido.getDireccion());
+    		Paquete p_dim_esp = new Paquete(id, pedido.getDireccion(), TipoPaquete.DIMESPECIALES);
     		id++;
-    		Paquete p_alimentacion = new Paquete(id, pedido.getDireccion());
+    		Paquete p_alimentacion = new Paquete(id, pedido.getDireccion(), TipoPaquete.ALIMENTACION);
     		id++;
     		for(Unidad u : pedido.getUnidades()) {
     			if(u.getEmpaquetado()==false) {
