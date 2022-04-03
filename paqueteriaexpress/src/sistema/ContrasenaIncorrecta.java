fa49aef426b58f2a7b52ae7bc36f69f6c2fedee6
paloba22 <pbasesio@gmail.com>
2022-04-03 19:00:18 +0200

    repartidor

--- paqueteriaexpress/src/sistema/ContrasenaIncorrecta.java
+++ paqueteriaexpress/src/sistema/ContrasenaIncorrecta.java
@@ -2,6 +2,11 @@ package sistema;
 
 public class ContrasenaIncorrecta extends Exception{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8708035574174757363L;
+
 	public ContrasenaIncorrecta() {
 		super("La contrasena es incorrecta.\n");
 	}
--- paqueteriaexpress/src/sistema/UsuarioNoEncontrado.java
+++ paqueteriaexpress/src/sistema/UsuarioNoEncontrado.java
@@ -1,6 +1,11 @@
 package sistema;
 
 public class UsuarioNoEncontrado extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -11487936203791678L;
+
 	public UsuarioNoEncontrado() {
 		super("El usuario no se ha encontrado\n");
 	}
