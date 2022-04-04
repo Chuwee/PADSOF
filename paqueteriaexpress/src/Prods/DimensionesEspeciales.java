d3045d86bdf579937e2c532791a4620a6151f53c
pinar02 <91214411+pinar02@users.noreply.github.com>
2022-04-04 11:58:57 +0200

    comentarios

--- paqueteriaexpress/src/Prods/DimensionesEspeciales.java
+++ paqueteriaexpress/src/Prods/DimensionesEspeciales.java
@@ -5,21 +5,45 @@ import GlobalVars.TipoPaquete;
  * 
  */
 import sistema.SistemaAplicacion;
+/**
+ * 
+ * esta clase hereda de la clase Producto
+ *
+ */
 public class DimensionesEspeciales extends Producto {
-
+/**
+ * Constructor de un producto de este tipo
+ * @param sist SistemaAicacion
+ * @param id, int, el identificador del producto
+ * @param peso, double, peso del producto
+ * @param precio, double, precio del producto
+ * @param descripcion, String, descripcion del producto
+ * @param unidades, int, número de unidades del producto
+ * @param largo, double
+ * @param ancho, double
+ * @param alto, double
+ */
 	public DimensionesEspeciales(SistemaAplicacion sist, int id, double peso, double precio, String descripcion, int unidades,
 			double largo, double ancho, double alto) {
 		super(sist, id, peso, descripcion, unidades, largo, ancho, alto);
 	}
-	
+	/**
+	 * Esta funcion va a devolver true, el producto es de dimensiones especiales
+	 */
 	@Override
 	public boolean isDimEsp() {
     	return true;
     }
+	/**
+	 * siemore va a devolver false, ya que no es de tiepo estandar
+	 */
 	@Override
     public boolean isEstandar() {
     	return false;
     }
+	/**
+	 * va a devolver el tipo de paquete dimensiones especiales, ya que el producto es de ese tipo
+	 */
 	@Override
 	public TipoPaquete getTipoPaquete() {
 		return TipoPaquete.DIMESPECIALES;
--- paqueteriaexpress/src/Prods/Liquido.java
+++ paqueteriaexpress/src/Prods/Liquido.java
@@ -5,13 +5,31 @@ package Prods;
  */
 import sistema.SistemaAplicacion;
 
-// Correcto 
+/**
+ * 
+ *esta clase hereda de la clase abstracta de productoAlimentacion
+ *
+ */
 
 public class Liquido extends ProductoAlimentacion {
-	
+	/**
+	 * Constructor
+	 * @param sist, Sistemaamplicacion
+	 * @param id, int, el identificador del producto
+	 * @param peso, double
+	 * @param precio double
+	 * @param descripcion String
+	 * @param unidades, int, las unidades que hay de ese producto
+	 * @param largo, double
+	 * @param ancho, double
+	 * @param alto, double
+	 */
 	public Liquido(SistemaAplicacion sist, int id, double peso, double precio, String descripcion, int unidades, double largo, double ancho, double alto) {
     	super(sist, id, peso, descripcion, unidades, largo, ancho, alto);
     }
+	/**
+	 * siempre va a devolver true
+	 */
     @Override
     public boolean isLiquido() {
     	return true;
