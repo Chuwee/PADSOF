4d71dbd084a0bc278c8c9aa1ce14e4e910e4b0f9
pinar02 <91214411+pinar02@users.noreply.github.com>
2022-04-04 11:34:44 +0200

    comentarios

--- paqueteriaexpress/src/Pedido/EstadoPedido.java
+++ paqueteriaexpress/src/Pedido/EstadoPedido.java
@@ -3,6 +3,11 @@ package Pedido;
  * @author Paloma Ballester Asesio, Ignacio Ildefonso del Miguel Ruano y María del Pinar Sacristán Matesanz
  * 
  */
+/**
+ * 
+ *esta enumeracion muestra los diferentes estados en los que puede estar un pedido
+ *
+ */
 public enum EstadoPedido {
 	EnConstruccion,
     EnReparto,
