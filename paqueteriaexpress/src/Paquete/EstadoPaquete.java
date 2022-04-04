55919476b638d1ed808e965b793a4559afdf39f8
pinar02 <91214411+pinar02@users.noreply.github.com>
2022-04-04 11:19:35 +0200

    comentarios

--- paqueteriaexpress/src/Paquete/EstadoPaquete.java
+++ paqueteriaexpress/src/Paquete/EstadoPaquete.java
@@ -3,6 +3,11 @@ package Paquete;
  * @author Paloma Ballester Asesio, Ignacio Ildefonso del Miguel Ruano y María del Pinar Sacristán Matesanz
  * 
  */
+/**
+ * 
+ * esta enumeracion muestra los diferentes estados de un paquete
+ *
+ */
 public enum EstadoPaquete {
 	EnReparto,
 	EnRepartoFallido1,
