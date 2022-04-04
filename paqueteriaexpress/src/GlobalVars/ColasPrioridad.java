e18c5d6a8e157c652865312a06472f964ee5e45d
pinar02 <91214411+pinar02@users.noreply.github.com>
2022-04-04 11:06:37 +0200

    comentarios

--- paqueteriaexpress/src/GlobalVars/ColasPrioridad.java
+++ paqueteriaexpress/src/GlobalVars/ColasPrioridad.java
@@ -3,6 +3,11 @@ package GlobalVars;
  * @author Paloma Ballester Asesio, Ignacio Ildefonso del Miguel Ruano y María del Pinar Sacristán Matesanz
  * 
  */
+/**
+ * 
+ * Esta es la enumeración para ver la prioridad de los paquetes
+ *
+ */
 public enum ColasPrioridad {
 	URGENTES,
 	NOENTREGADOSFALTACAMIONES,
--- paqueteriaexpress/src/GlobalVars/DimensionParam.java
+++ paqueteriaexpress/src/GlobalVars/DimensionParam.java
@@ -3,6 +3,11 @@ package GlobalVars;
  * @author Paloma Ballester Asesio, Ignacio Ildefonso del Miguel Ruano y María del Pinar Sacristán Matesanz
  * 
  */
+/**
+ * 
+ * esta enumeracion muestra las dimensiones que siempre tiene un producto
+ *
+ */
 public enum DimensionParam {
 	LARGO,
 	ANCHO,
